7bb7305167adb9a68ba772fe51f712db71478310
Julio Veronelli <julio.veronelli@joinmosaic.com>
2024-11-27 16:48:03 -0800

    Added API gateway endpoint.

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "lambda_sample_53_policy" {
+data "aws_iam_policy_document" "lambda_policy" {
   statement {
     effect = "Allow"
 
@@ -11,22 +11,54 @@ data "aws_iam_policy_document" "lambda_sample_53_policy" {
   }
 }
 
-resource "aws_iam_role" "lambda_sample_53_role" {
-  name               = "lambda_sample_53_role"
-  assume_role_policy = data.aws_iam_policy_document.lambda_sample_53_policy.json
+resource "aws_iam_role" "lambda_role" {
+  name               = "example_83_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_policy.json
 }
 
-data "archive_file" "lambda_sample_53_zip" {
+data "archive_file" "lambda_zip" {
   type        = "zip"
   source_file = "lambda.py"
   output_path = "lambda.zip"
 }
 
-resource "aws_lambda_function" "lambda_sample_53" {
+resource "aws_lambda_function" "lambda" {
   filename      = "lambda.zip"
-  function_name = "lambda_sample_53"
-  role          = aws_iam_role.lambda_sample_53_role.arn
+  function_name = "example_83_lambda"
+  role          = aws_iam_role.lambda_role.arn
   handler       = "lambda.lambda_handler"
-  source_code_hash = data.archive_file.lambda_sample_53_zip.output_base64sha256
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime = "python3.13"
+}
+
+resource "aws_apigatewayv2_api" "api" {
+  name          = "example_83_api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "stage" {
+  api_id = aws_apigatewayv2_api.api.id
+  name        = "sample"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "integration" {
+  api_id = aws_apigatewayv2_api.api.id
+  integration_uri    = aws_lambda_function.lambda.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "route" {
+  api_id = aws_apigatewayv2_api.api.id
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
+}
+
+resource "aws_lambda_permission" "api_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
---
+++ output.tf
@@ -0,0 +1,7 @@
+output "base_url" {
+  value = aws_apigatewayv2_stage.stage.invoke_url
+}
+
+output "url" {
+  value = "${aws_apigatewayv2_stage.stage.invoke_url}/hello"
+}
